a92403ccd5cdf491012d1ac08034201c46cd5a0f
Jlcarmen <johnluiscarmen.06.jlc@gmail.com>
2025-02-18 20:37:14 +0800

    Update Singly.py

---
+++ Singly.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+    
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end = " -> ")
+            temp = temp.next
+        print("None")
+
+#example usage
+ll = LinkedList()
+ll.insert("Pizza")
+ll.insert("Burger")
+ll.insert("Fries")
+ll.display()        
